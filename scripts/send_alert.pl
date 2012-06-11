11f3e7357773e7d78278748794b99000651ee64e
Robin Darby <robindarby@mac.com>
2012-06-11 09:50:36 -0700

    foo

---
+++ scripts/send_alert.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use lib qw(lib/perl);
+use Getopt::Long;
+use PerlGlue::APNS;
+
+my ($token, $msg);
+
+GetOptions('token=s' => \$token, 'message=s' => \$msg);
+
+my $apnsCert = "/home/darby/work/PerlGlue/conf/push_cert.pem";
+my $apnsKey  = "/home/darby/work/PerlGlue/conf/push_key.pem";
+
+my $apns = new PerlGlue::APNS(
+  cert        => $apnsCert,
+  key         => $apnsKey,
+  devicetoken => $token,
+  message     => $msg,
+  badge       => 0
+);
+
+$apns->write;
+
